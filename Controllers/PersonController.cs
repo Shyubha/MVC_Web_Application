15b68db8c4e98390aa2138e172ade14702f665bc
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-24 10:53:37 +0530

    PersonController And its operation added

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MVC_Web_Application.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public IActionResult PersonalDetails()
+        {
+            List<string> Options = new List<string>();
+            Options.Add("Choose an Option");
+            Options.Add("Yes");
+            Options.Add("No");
+            ViewData["Options"] = new SelectList(Options);
+            return View();
+
+
+        }
+        [HttpPost]
+
+        public ActionResult PersonalDetails(IFormCollection fc, ICollection<string> hobbies)
+        {
+            ViewBag.firstname = fc["firstname"];
+            ViewBag.lastname = fc["lastname"];
+            ViewBag.phone = fc["phone"];
+            ViewBag.gender = fc["Gender"];
+            ViewBag.hobbies = hobbies;
+            ViewBag.options = fc["Options"];
+            return View("Details");
+
+
+        }
+    }
+}
+    
