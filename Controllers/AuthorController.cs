9135b3355de748622a27af29fcf03e8441def58e
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-24 10:12:30 +0530

    Add project files.

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using MVC_Web_Application.Models;
+
+namespace MVC_Web_Application.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public IActionResult AuthorList()
+        {
+            List<Author> list = new List<Author>()
+            {
+            new Author { authorId = 22, authorName = "ShivajiSawant", authorBookName = "Mrityunjay" },
+            new Author { authorId = 33, authorName = "IravatiKarwe", authorBookName = "Mahabharat" },
+            new Author { authorId = 44, authorName = "Achyut Godbole", authorBookName = "Poetry" },
+            new Author { authorId = 77, authorName = "Salim Ali", authorBookName = "BirdView" },
+            };
+            ViewBag.Author = list;
+            return View();
+        }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Web_Application.Models
+{
+    public class Author
+    {
+        public int authorId { get; set; }
+
+        public string authorName { get; set; }
+
+        public string authorBookName { get; set; }
+    }
+}
